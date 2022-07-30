3a3550752e60a5788d10d40d2485b57cb316d832
= <=>
2022-07-31 01:23:25 +0700

    .

--- Venko/src/test/java/com/nlu/Venko/repository_test/ItemRepositoryTest.java
+++ Venko/src/test/java/com/nlu/Venko/repository_test/ItemRepositoryTest.java
@@ -4,20 +4,24 @@ import com.nlu.Venko.model.Item;
 import com.nlu.Venko.repository.ItemRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class ItemRepositoryTest {
 
-    @Autowired
+    @MockBean
     private ItemRepository itemRepository;
 
     @Test

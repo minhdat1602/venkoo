62e68c90b0e913b36acfb22a5f5c356cef98f823
= <=>
2022-07-31 01:31:21 +0700

    .

--- Venko/src/main/java/com/nlu/Venko/VenkoApplication.java
+++ Venko/src/main/java/com/nlu/Venko/VenkoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class VenkoApplication {
 
 	public static void main(String[] args) {

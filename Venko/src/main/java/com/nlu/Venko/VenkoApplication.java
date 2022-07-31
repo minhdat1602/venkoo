2a4bd15a6ef7106c70415181afef8bf55457a19b
thanhlong320 <nguyenthanhlong03022000@gmail.com>
2022-07-31 20:38:52 +0700

    Add CORS

--- Venko/src/main/java/com/nlu/Venko/VenkoApplication.java
+++ Venko/src/main/java/com/nlu/Venko/VenkoApplication.java
@@ -2,6 +2,10 @@ package com.nlu.Venko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class VenkoApplication {
@@ -9,5 +13,14 @@ public class VenkoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(VenkoApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
